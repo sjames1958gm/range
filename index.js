e6ba4f644ef42ebabdd9f1c78fcb0d3e81821b6d
Stephen James <sajames@1958@gmail.com>
2017-09-25 18:35:14 -0500

    buildup

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
 require('dotenv').config();
+
+var routes = require('./routes/index');
 
 let port = process.env.PORT | 8080;
 
@@ -12,6 +16,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
